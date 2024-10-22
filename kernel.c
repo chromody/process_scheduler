dae9edc7cf71a47e7a3970883aa8693a9b6d7e8f
chromody <chromody@gmail.com>
2024-10-22 14:03:37 -0500

    removed reference to queue.c from kernel

--- kernel.c
+++ kernel.c
@@ -5,7 +5,6 @@
 */
 #include <stdint.h>
 #include <stddef.h>
-#include "queue.c"
 #include "process.c"
 
 //--Prototypes -------------------------------------------------------------------------------------
