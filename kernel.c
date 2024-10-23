432f2a13f20d4ff3cc4abc8ef9cfb9127e1d83e3
chromody <chromody@gmail.com>
2024-10-22 19:42:02 -0500

    moved away from pointers for allocating

--- kernel.c
+++ kernel.c
@@ -42,7 +42,7 @@ int term_txtwidth(); //Prototype to find the maximum width of the terminal, defi
 
 
 int create_process();
-uint64_t *alloc_stack();
+uint64_t alloc_stack();
 PCB_t *alloc_pcb();
 void go();
 void enqueue(PCB_Q_t *q, PCB_t *pcb);
@@ -135,17 +135,17 @@ PCB_t *dequeue(PCB_Q_t *q) {
 }
 
 int create_process(int (*code_address)()) {
-	uint64_t *stackptr = alloc_stack();
+	uint64_t stackptr = alloc_stack();
 	if (stackptr == NULL) {return -1;}
 
 	uint64_t* sp = stackptr + STACK_SIZE;
 
 	for (int i = 0; i <= 32; ++i) {
 		--sp;
-		stackptr[*sp] = 0;
+		stackptr[sp] = 0;
 	}
 
-	stackptr[*sp+30] = (uint64_t)code_address;
+	stackptr[sp+30] = (uint64_t)code_address;
 	
 	PCB_t* pcb = alloc_pcb();
 	if (pcb == NULL) {return -1;}
@@ -158,7 +158,7 @@ int create_process(int (*code_address)()) {
 	return 0;
 }
 
-uint64_t* alloc_stack() {
+uint64_t alloc_stack() {
 	static int next_stack = 0;
 	if (readyQueue.size > MAX_PROCESSES) {
 		return NULL;
@@ -166,16 +166,16 @@ uint64_t* alloc_stack() {
 	return stacks[next_stack++];
 }
 
-PCB_t *alloc_pcb() {
+PCB_t* alloc_pcb() {
 	//static int next_process = 0;
 	if (readyQueue.size > MAX_PROCESSES) {
 		return NULL;
 	}
-	PCB_t *pcb;
-	pcb->pid = 0;
-	pcb->sp = 0;
-	pcb->next = NULL;
-	return pcb;
+	PCB_t pcb;
+	pcb.pid = 0;
+	pcb.sp = 0;
+	pcb.next = NULL;
+	return &pcb;
 }
 
 void go() {
