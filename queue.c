ec00abf644484723ebe7d3e77cd15709d3f7b7c9
chromody <chromody@gmail.com>
2024-10-24 18:04:31 -0500

    more cleaning up

--- queue.c
+++ queue.c
@@ -7,40 +7,42 @@
 
 //--Functions ----------------------------------------------------------------------------------------------------
 // Name: enqueue
-// Arguments: queue of process control blocks
-// Purpose: This will create a process. It will allocate and initialize its stack and pcb, then it will place that pcb on the ready queue.
+// Arguments: pointer to queue of process control blocks, and pointer to a process control block
+// Purpose: This will add a process's pcb to a queue of pcbs, which will be ready queue. It adds to the end of the queue
 void enqueue(PCB_Q_t *pcbQueue, PCB_t *pcb) {
-    if (pcbQueue->size >= MAX_PROCESSES) {
+    if (pcbQueue->size >= MAX_PROCESSES) { //check if our queue size is at its limit. But linked lists don't have a defined limit of size
         return; // Queue is full
     }
     
-    pcb->next = NULL; // Set the next pointer of the new PCB
+    pcb->next = NULL; //set the pcb's next pointer to null
 
-    if (pcbQueue->tail) {
-        pcbQueue->tail->next = pcb; // Link the new PCB at the end
-    } else {
-        pcbQueue->head = pcb; // Set head if queue is empty
+    if (pcbQueue->tail) { //if the queue is not empty
+        pcbQueue->tail->next = pcb; // Add the pcb to after the tail
+    } else { //if the queue is empty
+        pcbQueue->head = pcb; // set head to the pcb
     }
 
-    pcbQueue->tail = pcb; // Update the tail
-    pcbQueue->size++; // Increment the size of the queue
+    pcbQueue->tail = pcb; // Update the pcb's tail
+    pcbQueue->size++; // Incrementing the size of the pcb
 }
 
-// Dequeue a PCB from the ready queue
-PCB_t *dequeue(PCB_Q_t *pcbQueue) {
-    if (pcbQueue->size == 0) {
-        return NULL; // Queue is empty
+// Name: dequeue
+// Arguments: pointer to queue of process control blocks
+// Purpose: This will remove a pcb from the head of the queue
+PCB_t* dequeue(PCB_Q_t *pcbQueue) {
+    if (pcbQueue->size == 0) { // queue is empty so we return null
+        return NULL; 
     }
     
-    PCB_t *pcb = pcbQueue->head; // Get the head PCB
-    pcbQueue->head = pcb->next; // Move the head pointer to the next PCB
+    PCB_t *pcb = pcbQueue->head; // getting head of queue
+    pcbQueue->head = pcb->next; // moving the head up one pcb
     
-    if (pcbQueue->head == NULL) {
-        pcbQueue->tail = NULL; // Update tail if queue is now empty
+    if (pcbQueue->head == NULL) { // if the heade is now null, then it was the last pcb
+        pcbQueue->tail = NULL;
     }
 
-    pcbQueue->size--; // Decrement the size of the queue
-    pcb->next = NULL; // Clean up the next pointer
-    return pcb; // Return the dequeued PCB
+    pcbQueue->size--; // we removed a pcb so we are decrementing it
+    pcb->next = NULL; // no dangling pointers
+    return pcb; // returning the pcb now
 }
 //----------------------------------------------------------------------------------------------------------------
